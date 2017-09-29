27b949b2c053999644a213b6a2fa5b75ce331ee7
Khristopher Chireno <kcmastermind2005@gmail.com>
2017-09-29 14:28:48 +0000

    GuessWord working but not really pythonic

--- hangman/game.py
+++ hangman/game.py
@@ -22,19 +22,35 @@ class GuessAttempt(object):
 
 class GuessWord(object):
     
-    def __init__(self, answer):
-        
+    def __init__(self, answer = None):
+        # Return invalid if answer is empty
         if not answer:
             raise InvalidWordException()
-            
         self.answer = answer
         self.masked =''.join(['*' for i in answer])
+        self.masked_list = list(self.masked)
+        self.hit = bool
+        self.miss = bool
     
     # This needs to take in the object from GuessAttempt and look at the attmept.letter so it can return GuessAttempt.hit()
     # or miss() will wok on that one next
     def perform_attempt(self, attempt):
-        if attempt > 1 or not attempt :
+        if len(attempt) > 1 or not attempt :
             raise InvalidGuessedLetterException()
+            
+        if attempt.lower() in self.answer.lower():
+            hit_locations = [i for i in range(len(self.answer))
+            if attempt.lower() ==self.answer[i].lower()]
+            
+            for i in hit_locations:
+                self.masked_list[i] = attempt.lower()
+            self.masked = ''.join(self.masked_list)
+            self.hit = True
+        else:
+            self.hit = False
+        self.miss = not self.hit
+        
+        return(GuessAttempt(attempt, self.hit or None, self.miss or None))
     
 
 class HangmanGame(object):
