20a51c7348a081e410c43118d1fbece6e8a6e93d
Khristopher Chireno <kcmastermind2005@gmail.com>
2017-09-29 13:03:00 +0000

    first 3 tests work for GuessWord

--- hangman/game.py
+++ hangman/game.py
@@ -6,8 +6,21 @@ class GuessAttempt(object):
 
 
 class GuessWord(object):
-    pass
-
+    
+    def __init__(self, answer):
+        
+        if not answer:
+            raise InvalidWordException()
+            
+        self.answer = answer
+        self.masked =''.join(['*' for i in answer])
+    
+    # This needs to take in the object from GuessAttempt and look at the attmept.letter so it can return GuessAttempt.hit()
+    # or miss() will wok on that one next
+    def perform_attempt(self, attempt):
+        if attempt > 1 or not attempt :
+            raise InvalidGuessedLetterException()
+    
 
 class HangmanGame(object):
     pass
